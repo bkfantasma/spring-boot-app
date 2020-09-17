058f2f34ef62ec067e93cb26fcb8500ba770f634
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-09-16 23:57:08 -0300

    first commit

---
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,12 @@
+package br.gov.sp.fatec.springbootapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springbootapp/Test.java
@@ -0,0 +1,5 @@
+package br.gov.sp.fatec.springbootapp;
+
+public @interface Test {
+
+}
