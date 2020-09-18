6fd64e197f4414086f2ad6685527f78bba23f706
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-09-18 03:52:08 +0000

    test

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.assertNotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.springbootapp.Entity.Produto;
 import br.gov.sp.fatec.springbootapp.Repository.ProdutoRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
 
     @Autowired
