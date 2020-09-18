5dbc6fbde3d80ee11882d88fe9f49b0afb348d7c
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-09-18 02:55:27 +0000

    a

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,12 +1,29 @@
 package br.gov.sp.fatec.springbootapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.gov.sp.fatec.springbootapp.Entity.Produto;
+import br.gov.sp.fatec.springbootapp.Repository.ProdutoRepository;
 
 @SpringBootTest
 class SpringBootAppApplicationTests {
 
+    @Autowired
+    private ProdutoRepository produtoRepo;
+
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void testeInsercao() {
+        Produto produto = new Produto();
+        produto.setDescricao("embalagem");
+        produto.setValorCompra(10,00);
+        produtoRepo.save(produto);
+
+        
+    }
 
 }
