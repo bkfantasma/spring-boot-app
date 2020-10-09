2efe8745ad846da97f6daa5c915389cc8852aff5
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-10-09 20:06:30 +0000

    n

--- src/main/java/br/gov/sp/fatec/springbootapp/repository/ProdutoRepository.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/repository/ProdutoRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.gov.sp.fatec.springbootapp.entity.Produto;
-//import javafx.scene.chart.PieChart.Data;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
