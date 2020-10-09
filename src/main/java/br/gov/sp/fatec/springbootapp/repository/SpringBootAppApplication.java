75f17241bf9d41d2b77abeb8412514313438abbc
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-10-09 13:35:16 +0000

    join

--- src/main/java/br/gov/sp/fatec/springbootapp/repository/SpringBootAppApplication.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/repository/SpringBootAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
