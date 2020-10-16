8495e61281856ae676eccca1ca16e1d37486eb8e
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-10-16 13:45:48 +0000

    resturn json

--- src/main/java/br/gov/sp/fatec/springbootapp/controller/ProdutoController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package br.gov.sp.fatec.springbootapp.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,13 @@ import br.gov.sp.fatec.springbootapp.service.SegurancaService;
 
 @RestController
 @RequestMapping(value = "/produto")
+@CrossOrigin
 public class ProdutoController {
 
+    @Autowired
     private SegurancaService segurancaService;
 
-    @GetMapping(value = "/todos")
+    @GetMapping
     public List<Produto> buscarTodosProdutos() {
         return segurancaService.buscarTodosProdutos();
     }
