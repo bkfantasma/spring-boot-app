563cfb8b5a2e5478e62aa8cd329a8c7424054712
bkfantasma <bruna_cristina_lopes@hotmail.com>
2020-10-19 16:06:12 +0000

    h

--- DDL.sql
+++ DDL.sql
@@ -3,22 +3,24 @@ create schema estoqueMinusculo;
 use estoqueMinusculo;
 
 create table produto (
-id bigint not null auto_increment,
+id bigint(255) not null auto_increment,
 descricao varchar(255) not null,
 valor_compra float (10,2),
 valor_venda float (10,2),
 status boolean not null,
--- id_estoque bigint,
-primary key (id)
--- foreign key id_estoque_fk (id_estoque) references estoque(id) 
+id_estoque bigint (255),
+primary key (id),
+
+-- alter table produto add constraint id_estoque_fk foreign key (id_estoque) references estoque(id)
+
 );
 
 create table estoque (
-id bigint not null auto_increment,
+id bigint (255) not null auto_increment,
 data_entrada_produto timestamp not null,
 data_saida_produto timestamp not null,
 quantidade bigint not null,
-id_produto bigint,
+id_produto bigint (255),
 primary key (id),
 foreign key id_produto_fk (id_produto) references produto(id) 
 );
